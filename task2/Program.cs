665ad31420cec5c8739a2c31d69ec26823ea9a3c
«Ann» <annapt69@mail.ru>
2022-09-29 21:14:15 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и 
+выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число");
+string userNumber = Console.ReadLine();
+char[] array = userNumber.ToCharArray();
+int sum = 0;
+Console.WriteLine($"{array}");
+
+for (int i = 0; i < array.Length; i++)
+{
+    sum = array[i] + sum;
+}
+
+Console.WriteLine($"{sum}");
