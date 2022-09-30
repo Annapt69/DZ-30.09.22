e5bf4a7440a40359f70b830ddbf50195ef6226b8
«Ann» <annapt69@mail.ru>
2022-09-30 13:23:34 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 29: Напишите программу, в которой пользователь задает длину массива, 
+элементы которого задаются в диапазоне [1,99] и выводит на экран .
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+
+Console.WriteLine("Задайте длину массива");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[userNumber];
+userNumber = array.Length;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 99);
+}
+printArray(array);
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+           Console.Write(","); 
+        }
+    }
+    Console.Write("]");
+}
+Console.WriteLine();
